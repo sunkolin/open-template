12a4e8654e8390fc402a87035f70950992418ff7
sunkolin <sunkolin@qq.com>
2026-04-29 11:25:02 +0800

    用户管理页面查询用户列表功能，用户列表中时间显示格式调整

--- frontend/src/views/user/index.vue
+++ frontend/src/views/user/index.vue
@@ -262,7 +262,8 @@ export default {
       const day = String(d.getDate()).padStart(2, '0')
       const hours = String(d.getHours()).padStart(2, '0')
       const minutes = String(d.getMinutes()).padStart(2, '0')
-      return `${year}-${month}-${day} ${hours}:${minutes}`
+      const seconds = String(d.getSeconds()).padStart(2, '0')
+      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
     }
   }
 }
