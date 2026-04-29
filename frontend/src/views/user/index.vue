79db7f4976f18e55d63430ae36b8a945e92ae9c2
sunkolin <sunkolin@qq.com>
2026-04-29 13:59:52 +0800

    fix

--- frontend/src/views/user/index.vue
+++ frontend/src/views/user/index.vue
@@ -41,12 +41,10 @@
 
     <!-- 分页 -->
     <el-pagination
-      @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :current-page="currentPage"
-      :page-sizes="[10, 20, 50, 100]"
       :page-size="pageSize"
-      layout="total, sizes, prev, pager, next, jumper"
+      layout="total, prev, pager, next"
       :total="total"
       style="margin-top: 20px; text-align: right;"
     />
